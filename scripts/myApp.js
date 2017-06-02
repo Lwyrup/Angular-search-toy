a36e504a0c8b31352e07bd408b96f974ea5c2911
Spencer Gerdes <skgerdes@yahoo.com>
2017-06-02 11:39:54 -0500

    Added data to loop through:

--- scripts/myApp.js
+++ scripts/myApp.js
@@ -2,4 +2,13 @@ var app = angular.module('searchApp', []);
 
 app.controller('myCtrl', function($scope) {
     $scope.search = "John";
+    $scope.people = [
+	    {fname:"Steve", lname:"Austin"},
+	    {fname:"Mark", lname:"Hamil"},
+	    {fname:"Mark", lname:"Zuckerberg"},
+	    {fname:"Jeff", lname:"Kaplan"},
+	    {fname:"John", lname:"Smith"},
+	    {fname:"Fred", lname:"Durst"},
+	    {fname:"Demetri", lname:"Martin"}
+    ];
 });
